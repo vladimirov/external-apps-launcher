1c63c4cf1c8015cf81e0ee8e00f8e16cbdc10ce4
vladimirov <denis.vladimiroff@gmail.com>
2018-11-05 18:47:39 +0200

    first commit

--- src/test/java/LaunchingExternalApps.java
+++ src/test/java/LaunchingExternalApps.java
@@ -9,12 +9,9 @@ public class LaunchingExternalApps {
 
     public static void main(String[] args) throws InterruptedException, IOException {
         LaunchingExternalApps obj = new LaunchingExternalApps();
-//        String output = obj.executeCommand("docker ps");
-
-        if (!obj.executeCommand("docker ps").contains("CONTAINER")){
+        if (!obj.executeCommand("docker ps").contains("CONTAINER")) {
             File file = new File("C:\\Program Files\\Docker\\Docker\\Docker for Windows.exe");
             Desktop.getDesktop().open(file);
-
             while (!obj.executeCommand("docker ps").contains("CONTAINER")) {
                 Thread.sleep(5000);
                 System.out.println("Docker is starting...");
